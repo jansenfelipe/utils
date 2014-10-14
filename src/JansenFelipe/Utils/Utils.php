74b241b11b45a30d9b03113f502ace18db0c7a76
Jansen Felipe <jansen.felipe@gmail.com>
2014-10-14 17:07:29 -0300

    Implementação isCpf e Correções

--- src/JansenFelipe/Utils/Utils.php
+++ src/JansenFelipe/Utils/Utils.php
@@ -19,7 +19,7 @@ class Utils {
 
         if (empty($txt))
             return '';
-        $txt = $this->unmask($txt);
+        $txt = self::unmask($txt);
         $qtd = 0;
         for ($x = 0; $x < strlen($mascara); $x++) {
             if ($mascara[$x] == "#")
@@ -70,45 +70,75 @@ class Utils {
      * @return boolean
      */
     public static function isCnpj($cnpj) {
-        $cnpj = self::unmask($cnpj);
-        
-        if (empty($cnpj) || $cnpj == '00000000000000')
-            return false;
+        $valid = true;
+        $cnpj = str_pad(self::unmask($cnpj), 14, '0', STR_PAD_LEFT);
 
-        $dig_1 = 0;
-        $dig_2 = 0;
-        $controle_1 = 5;
-        $controle_2 = 6;
-        $resto = 0;
-
-        for ($i = 0; $i < 12; $i++) {
-            $dig_1 = $dig_1 + (double) (substr($cnpj, $i, 1) * $controle_1);
-            $controle_1 = $controle_1 - 1;
-            if ($i == 3)
-                $controle_1 = 9;
+        for ($x = 0; $x < 10; $x++) {
+            if ($cnpj == str_repeat($x, 14)) {
+                $valid = false;
+            }
         }
 
-        $resto = $dig_1 % 11;
-        $dig_1 = 11 - $resto;
-        if (($resto == 0) || ($resto == 1))
-            $dig_1 = 0;
-
-        for ($i = 0; $i < 12; $i++) {
-            $dig_2 = $dig_2 + (int) (substr($cnpj, $i, 1) * $controle_2);
-            $controle_2 = $controle_2 - 1;
-            if ($i == 4)
-                $controle_2 = 9;
+        if ($valid) {
+            if (strlen($cnpj) != 14) {
+                $valid = false;
+            } else {
+                for ($t = 12; $t < 14; $t ++) {
+                    $d = 0;
+                    $c = 0;
+                    for ($m = $t - 7; $m >= 2; $m --, $c ++) {
+                        $d += $cnpj {$c} * $m;
+                    }
+                    for ($m = 9; $m >= 2; $m --, $c ++) {
+                        $d += $cnpj {$c} * $m;
+                    }
+                    $d = ((10 * $d) % 11) % 10;
+                    if ($cnpj {$c} != $d) {
+                        $valid = false;
+                        break;
+                    }
+                }
+            }
         }
 
-        $dig_2 = $dig_2 + (2 * $dig_1);
-        $resto = $dig_2 % 11;
-        $dig_2 = 11 - $resto;
+        return $valid;
+    }
 
-        if (($resto == 0) || ($resto == 1))
-            $dig_2 = 0;
+    /**
+     * Metodo para verificar se um CPF é válido
+     * 
+     * @param  string $cpf
+     * @return boolean
+     */
+    public static function isCpf($cpf) {
+        $valid = true;
+        $cpf = str_pad(self::unmask($cpf), 11, '0', STR_PAD_LEFT);
 
-        $dig_ver = ($dig_1 * 10) + $dig_2;
-        return $dig_ver == (double) (substr($cnpj, strlen($cnpj) - 2, 2));
+        for ($x = 0; $x < 10; $x ++) {
+            if ($cpf == str_repeat($x, 11)) {
+                $valid = false;
+            }
+        }
+
+        if ($valid) {
+            if (strlen($cpf) != 11) {
+                $valid = false;
+            } else {
+                for ($t = 9; $t < 11; $t ++) {
+                    $d = 0;
+                    for ($c = 0; $c < $t; $c ++) {
+                        $d += $cpf {$c} * (($t + 1) - $c);
+                    }
+                    $d = ((10 * $d) % 11) % 10;
+                    if ($cpf{$c} != $d) {
+                        $valid = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return $valid;
     }
 
 }
