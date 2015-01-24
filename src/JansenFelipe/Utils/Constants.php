9608284f51df5fcdf5a56c042ca282bca173622e
Jansen Felipe <jansen.felipe@gmail.com>
2015-01-24 17:30:31 -0200

    Update

---
+++ src/JansenFelipe/Utils/Constants.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace JansenFelipe\Utils;
+
+interface Constants {
+
+    const TELEFONE = '8 OU 9 DIGITOS';
+    const DOCUMENTO = 'CPF OU CNPJ';
+    const CPF = '###.###.###-##';
+    const CNPJ = '##.###.###/####-##';
+    const CEP = '##.###-###';
+
+}
